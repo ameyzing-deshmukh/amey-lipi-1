d2f46f163efa8f623b1989265385bec05c97e9e5
amey_deshmukh <ameydeshmukh1990@gmail.com>
2020-07-29 00:59:07 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { KeypadComponent } from './keypad/keypad.component';
+import { InformationComponent } from './information/information.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    KeypadComponent,
+    InformationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
